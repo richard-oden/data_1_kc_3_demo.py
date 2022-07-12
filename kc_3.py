6c2dfce56d894af90b55650eceb039074299a852
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-12 18:46:00 -0400

    Refactored lines 30-33.

--- kc_3.py
+++ kc_3.py
@@ -27,10 +27,7 @@ with open("website_data.json", 'r') as open_file:
     website_dicts = json.load(open_file)
 
 # Call our functions.
-green_websites_dicts = []
-for website in website_dicts:
-    if is_website_green(website) == True:
-        green_websites_dicts.append(website)
+green_websites_dicts = filter(lambda website: is_website_green(website) == True, website_dicts)
 
 percentage_clean_websites = (len(green_websites_dicts) / len(website_dicts)) * 100
 print(f'{percentage_clean_websites}% of tested websites were green.')
@@ -38,3 +35,5 @@ print(f'{percentage_clean_websites}% of tested websites were green.')
 sorted_website_dicts = sort_by_cleanliness(website_dicts)
 for website_dict in sorted_website_dicts:
     print(f'{website_dict["url"]}: {website_dict["cleanerThan"]}')
+
+    
